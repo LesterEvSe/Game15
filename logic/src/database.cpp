189ffa3f099d812c5acdafee779886dc48d29106
zhenia <evgenii.nikolaevichfgh@gmail.com>
2023-06-22 22:36:29 +0300

    SQL query 'top 10 for each table dimension' completed

--- logic/src/database.cpp
+++ logic/src/database.cpp
@@ -11,14 +11,16 @@ Database::Database() :
     if (!m_stats.open())
         qDebug() << "Failed to connect to database: " << m_stats.lastError().text();
 
-    // table - for which dimensionality of the field the result
+    // A semicolon at the end of the query
+    // is needed to separate multiple queries.
+    // If the query is only 1, it is not necessary
     QSqlQuery query("SELECT name FROM sqlite_master WHERE type='table' AND name='stats';");
     if (!query.next() &&
             !query.exec("CREATE TABLE stats ("
                         "id INTEGER PRIMARY KEY AUTOINCREMENT, "
                         "dimension INTEGER NOT NULL, "
                         "time INTEGER NOT NULL, "
-                        "username TEXT NOT NULL);"))
+                        "username TEXT NOT NULL)"))
         qDebug() << "Failed to create stats table: " << query.lastError().text();
 }
 
@@ -38,35 +40,23 @@ void Database::add_time(int dimension, unsigned int time_sec, const QString &nic
     if (!query.exec())
         throw QSqlError(query.lastError().text());
 
-    // 1. Create temp table
-    if (!query.exec("CREATE TABLE temp ("
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    "dimension INTEGER NOT NULL, "
-                    "time INTEGER NOT NULL, "
-                    "username TEXT NOT NULL);"))
-        throw QSqlError(query.lastError().text());
+    // ROW_NUMBER() - Analitical func. Assign an ordinal number to each line
+    // OVER (...) - How is the separation performed
+    // PARTITION BY - Analog to GROUP BY, only for analytical functions
+    query.prepare("DELETE FROM stats "
+                  "WHERE id IN ( "
+                  "  SELECT id FROM ( "
+                  "    SELECT id, "
+                  "      ROW_NUMBER() OVER "
+                  "      (PARTITION BY dimension ORDER BY time ASC) "
+                  "      AS row_number "
+                  "    FROM stats "
+                  "    WHERE dimension = :dimension "
+                  "  ) WHERE row_number > 10 "
+                  ") AND dimension = :dimension");
 
-    // 2. Insert sorted data to temporary table
-    if (!query.exec("INSERT INTO temp (dimension, time, username) "
-                    "SELECT dimension, time, username FROM stats "
-                    "ORDER BY time ASC;"))
-        throw QSqlError(query.lastError().text());
-
-    // 3. Delete original table
-    if (!query.exec("DROP TABLE stats;"))
-        throw QSqlError(query.lastError().text());
-
-    // 4. Rename temp table
-    if (!query.exec("ALTER TABLE temp RENAME TO stats;"))
-        throw QSqlError(query.lastError().text());
-
-    // NEED TO FIX!!!
-    // For different dimensions, different results,
-    // you need a limit of no more than 100 for each,
-    // not all at the same time
-
-    // 5. Apply constraint
-    if (!query.exec("DELETE FROM stats WHERE id > 100;"))
+    query.bindValue(":dimension", dimension);
+    if (!query.exec())
         throw QSqlError(query.lastError().text());
 }
 
@@ -75,7 +65,7 @@ std::vector<std::vector<QString>> Database::get_time_players(int dimension)
 {
     QSqlQuery query("SELECT time, username "
                     "FROM stats "
-                    "WHERE dimension = :dim;");
+                    "WHERE dimension = :dim");
     query.bindValue(":dim", dimension);
 
     if (!query.exec())
